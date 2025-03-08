56027134a49f3389ce14c0117acff2b4c2ad3e5e
habilithm97 <habinkim97@gmail.com>
2025-03-08 23:07:03 +0900

    feat: Memo를 Parcelable로 변경

--- app/src/main/java/com/example/memorynote2025/room/memo/Memo.kt
+++ app/src/main/java/com/example/memorynote2025/room/memo/Memo.kt
@@ -1,12 +1,15 @@
 package com.example.memorynote2025.room.memo
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize // Parcelable 자동 구현
 @Entity(tableName = "memos")
 data class Memo(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     @ColumnInfo(name = "content") var content: String,
     @ColumnInfo(name = "date") var date: Long
-)
+) : Parcelable // 컴포넌트 간에 데이터를 전달할 수 있도록 직렬화
