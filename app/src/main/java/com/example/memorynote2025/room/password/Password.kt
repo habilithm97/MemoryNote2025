3f388da6a2906a92468a4c0e17300760eb744d56
habilithm97 <habinkim97@gmail.com>
2025-03-18 15:43:41 +0900

    Password Entity 추가

---
+++ app/src/main/java/com/example/memorynote2025/room/password/Password.kt
@@ -0,0 +1,10 @@
+package com.example.memorynote2025.room.password
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Password(
+    @PrimaryKey val id: Int = 1, // 항상 한 개의 비밀번호만 저장
+    val password: String
+)
