cfa1e77909714aaf80cc4fd533cc2223b6920566
habilithm97 <habinkim97@gmail.com>
2025-04-15 23:29:45 +0900

    메모 Repository 및 ViewModel 정리

--- app/src/main/java/com/example/memorynote2025/repository/MemoRepository.kt
+++ app/src/main/java/com/example/memorynote2025/repository/MemoRepository.kt
@@ -4,12 +4,6 @@ import com.example.memorynote2025.room.memo.Memo
 import com.example.memorynote2025.room.memo.MemoDao
 import kotlinx.coroutines.flow.Flow
 
-/* Repository
- -데이터 소스 추상화 -> ViewModel은 데이터를 가져오는 방법에 대해 알 필요 없이
-   Repository를 통해 데이터 처리 가능
- -비즈니스 로직 캡슐화 -> 중복 감소, 유지 보수 용이
- -ViewModel과 데이터 소스 간의 중재자 역할 -> 의존성 최소화
- */
 class MemoRepository(private val memoDao: MemoDao) {
     suspend fun insertMemo(memo: Memo) {
         memoDao.insertMemo(memo)
@@ -24,3 +18,12 @@ class MemoRepository(private val memoDao: MemoDao) {
         return memoDao.getAll()
     }
 }
+/*
+* Repository
+ -데이터 소스 추상화 -> ViewModel은 데이터 출처를 몰라도 됨
+ -비즈니스 로직 캡슐화 -> 중복 감소, 유지 보수 용이
+ -ViewModel과 데이터 소스 간의 중재자 -> 의존성 최소화
+
+* 데이터 소스 : 데이터를 실제로 저장하거나 불러오는 곳 (Room 등)
+* 비즈니스 로직 : 특정 기능을 수행하기 위해 처리하는 로직 (정렬 순서 변경 등)
+ */
--- app/src/main/java/com/example/memorynote2025/viewmodel/MemoViewModel.kt
+++ app/src/main/java/com/example/memorynote2025/viewmodel/MemoViewModel.kt
@@ -11,7 +11,7 @@ import com.example.memorynote2025.room.memo.MemoDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-// Context는 액/프 종료 시 사라지지만, Application은 앱 생명주기와 함께 유지
+// Context는 UI 컴포넌트 종료 시 사라지지만, Application은 앱 생명주기와 함께 유지
 class MemoViewModel(application: Application) : AndroidViewModel(application) {
     private val memoRepository: MemoRepository
     val getAll: LiveData<List<Memo>>
@@ -19,8 +19,7 @@ class MemoViewModel(application: Application) : AndroidViewModel(application) {
     init {
         val memoDao = MemoDatabase.getInstance(application).memoDao()
         memoRepository = MemoRepository(memoDao)
-        // UI에서 쉽게 관찰할 수 있도록 Flow를 LiveData로 변환
-        // UI는 LiveData만 관찰하고, 데이터 흐름은 그대로 유지 가능
+        // UI에서 쉽게 관찰할 수 있도록 LiveData로 변환
         getAll = memoRepository.getAll().asLiveData()
     }
 
@@ -42,3 +41,13 @@ class MemoViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 }
+/*
+* AAC ViewModel
+ -AndroidViewModel은 UI 컴포넌트보다 오래 살아남고, Application Context 사용 가능
+ -viewModelScope는 ViewModel 생명주기에 맞춰 비동기 작업을 안전하게 관리
+
+* MVVM ViewModel
+ -UI와 데이터 간의 중재자
+ -LiveData를 통해 UI에 데이터를 제공
+ -Repository를 통해 비즈니스 로직을 처리
+ */
